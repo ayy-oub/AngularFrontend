026364347b7253360fb8dd4f335e077d989d9ffb
ayy-oub <hajjajiayyoub@gmail.com>
2021-04-10 17:59:35 +0100

    Angular Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CatalogueService } from './catalogue.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularfrontend';
+export class AppComponent implements OnInit {
+
+  categories;
+  currentCategory;
+
+
+  constructor(private catalogueService: CatalogueService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  private getCategories() {
+    this.catalogueService.getResource("/categories")
+      .subscribe(data => {
+        this.categories = data;
+      }, error => {
+        console.log(error);
+      })
+  }
+
+  getProductsByCat(c) {
+    this.currentCategory = c;
+    this.router.navigateByUrl('/products/2/' + c.id);
+  }
+
+  onSelectedProducts() {
+    this.currentCategory = undefined;
+    this.router.navigateByUrl('/products/1/0');
+  }
 }
